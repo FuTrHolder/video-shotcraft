944645cfca999073758ef1f79b4dd1524906ee92
JW_Bear <dmenc.hjw@gmail.com>
2026-09-01 13:18:56 +0900

    Refactor RootWithBlog to use React.createElement
    Refactor RootWithBlog component to use React.createElement.

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,14 +1,14 @@
-import { registerRoot } from "remotion";
-import { Root } from "./Root";
-import { BlogRoot } from "./BlogRoot";
+import React from "react";
+import {registerRoot} from "remotion";
+import {Root} from "./Root";
+import {BlogRoot} from "./BlogRoot";
 
-const RootWithBlog: React.FC = () => {
-  return (
-    <>
-      <Root />
-      <BlogRoot />
-    </>
+const RootWithBlog: React.FC = () =>
+  React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Root),
+    React.createElement(BlogRoot),
   );
-};
 
 registerRoot(RootWithBlog);
